fead4aa370b25d3908f9188dbcd159efcb533168
musketeer90 <goteamspeak90>
2015-06-03 01:44:42 +0200

    Recovery Manager
    Läuft !

--- Aufgabe5/src/FileRead.java
+++ Aufgabe5/src/FileRead.java
@@ -1,7 +1,6 @@
-import java.io.*;
-import java.util.*;
-
-import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /**
  * class responsible for reading data out of the specified files;
@@ -9,5 +8,30 @@ import javax.swing.JOptionPane;
 public class FileRead {
 
 	private BufferedReader reader;
+	
+	/**
+	 * Set current working directory with directory
+	 * @param directory
+	 */
+	FileRead(String filepath){
+		
+		try{
+			this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(filepath)));
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String readData(){
+		try{
+			return reader.readLine();
+		}catch(Exception e){
+			e.printStackTrace();
+			return e.toString();
+		}
+		
+	}
 
 }
